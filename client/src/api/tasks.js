f21c2472446aab4c87d6b8360cad8f08a583ecba
Pranav <gargpranav851@gmail.com>
2026-05-26 00:24:51 +0530

    feat(client): add task api helper module

---
+++ client/src/api/tasks.js
@@ -0,0 +1,7 @@
+import API from './axios';
+
+export const fetchAllTasks = () => API.get('/tasks');
+export const createTask = (data) => API.post('/tasks', data);
+export const updateTask = (id, data) => API.put(`/tasks/${id}`, data);
+export const deleteTask = (id) => API.delete(`/tasks/${id}`);
+export const fetchTalents = () => API.get('/users/talents');
